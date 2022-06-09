64edfd3a00ddb8cb156179f772cf54ebf20fccfe
Krzysztof Bednarski <bednarski1978@gmail.com>
2022-06-09 20:20:05 +0200

    UserMonthStatistics

--- config/bundles.php
+++ config/bundles.php
@@ -10,6 +10,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['dev' => true, 'test' => true],
 ];
